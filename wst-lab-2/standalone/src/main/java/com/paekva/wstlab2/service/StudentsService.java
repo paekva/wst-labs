3e372ed13f8e07fbced824ec172e42c03ae086aa
paekva <paekva@yandex.ru>
2021-03-22 22:59:20 +0300

    lab 2: added CRUD to standalone app layer

--- wst-lab-2/standalone/src/main/java/com/paekva/wstlab2/service/StudentsService.java
+++ wst-lab-2/standalone/src/main/java/com/paekva/wstlab2/service/StudentsService.java
@@ -4,6 +4,7 @@ import com.paekva.wstlab2.database.StudentDAO;
 import com.paekva.wstlab2.database.entity.Student;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -15,22 +16,46 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentsService {
-  private StudentDAO studentDAO;
+    private StudentDAO studentDAO;
 
-  @WebMethod
-  public List<Student> findAll() throws SQLException {
-    return studentDAO.findAll();
-  }
+    @WebMethod
+    public List<Student> findAll() throws SQLException {
+        return studentDAO.findAll();
+    }
 
-  @WebMethod
-  public List<Student> findWithFilters(
-      @WebParam(name = "id") Long id,
-      @WebParam(name = "email") String email,
-      @WebParam(name = "password") String password,
-      @WebParam(name = "groupNumber") String groupNumber,
-      @WebParam(name = "isLocal") Boolean isLocal,
-      @WebParam(name = "birthDate") XMLGregorianCalendar birthDate
-  ) throws SQLException {
-    return studentDAO.findWithFilters(id, groupNumber, password, email, isLocal, birthDate);
-  }
+    @WebMethod
+    public List<Student> findWithFilters(
+            @WebParam(name = "id") Long id,
+            @WebParam(name = "email") String email,
+            @WebParam(name = "password") String password,
+            @WebParam(name = "groupNumber") String groupNumber,
+            @WebParam(name = "isLocal") Boolean isLocal,
+            @WebParam(name = "birthDate") XMLGregorianCalendar birthDate
+    ) throws SQLException {
+        return studentDAO.findWithFilters(id, groupNumber, password, email, isLocal, birthDate);
+    }
+
+    @WebMethod
+    public boolean delete(@WebParam(name = "id") Long id) throws SQLException {
+        return studentDAO.delete(id);
+    }
+
+    @WebMethod
+    public Long insert(
+            @WebParam(name = "email") String email, @WebParam(name = "password") String password,
+            @WebParam(name = "groupNumber") String groupNumber, @WebParam(name = "isLocal") Boolean isLocal,
+            @WebParam(name = "birthDate") XMLGregorianCalendar birthDate
+    ) throws SQLException {
+        return studentDAO.insert(email, password, groupNumber, isLocal, birthDate);
+    }
+
+    @WebMethod
+    public boolean update(
+            @WebParam(name = "id") Long id,
+            @WebParam(name = "email") String email, @WebParam(name = "password") String password,
+            @WebParam(name = "groupNumber") String groupNumber, @WebParam(name = "isLocal") Boolean isLocal,
+            @WebParam(name = "birthDate") XMLGregorianCalendar birthDate
+    ) throws SQLException {
+        return studentDAO.update(id, email, password, groupNumber, isLocal, birthDate);
+    }
 }
